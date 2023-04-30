12a4030b52c0ce548782fcfd5545005abdb63229
junsoopooh <junsoopooh@Naver.com>
2023-04-30 22:33:54 +0900

    recurrsion

--- week04/junsu/2_5568.py
+++ week04/junsu/2_5568.py
@@ -4,27 +4,35 @@ n = int(sys.stdin.readline())
 k = int(sys.stdin.readline())
 cards = []
 for _ in range(n):
-    card = int(sys.stdin.readline())
+    card = sys.stdin.readline().rstrip()
     cards.append(card)
-arr=[]
-def draw(y, arr):
-    x = cards.po
-    arr.append(x)
-    y -= 1
-    if k != 0:
-        draw(y,arr)
-    else:
-        return
 
-new_arr=draw(k,arr)
 ans = set()
-for i in range(len(new_arr)):
-    a = new_arr[i]
-    new_arr.remove(a)
-    for j in range(len(new_arr)):
-        b = new_arr[j]
-        new_word = ''.join(a+b)
-        new_word_2= ''.join(b+a)
-        ans.add(new_word)
-        ans.add(new_word_2)
+if k == 2:
+    for i in cards:
+        for j in cards:
+            if cards.index(i) != cards.index(j):
+                new_word1 = ''.join([i,j])
+                new_word2 = ''.join([j,i])
+                ans.add(new_word1)
+                ans.add(new_word2)
+elif k == 3:
+    for i in cards:
+        for j in cards:
+            if cards.index(i) != cards.index(j):
+                for l in cards:
+                    if cards.index(i) != cards.index(l) and cards.index(j) != cards.index(l):
+                        new_word1 = ''.join([i,j,l])
+                        new_word2 = ''.join([i,l,j])
+                        new_word3 = ''.join([j,i,l])
+                        new_word4 = ''.join([j,l,i])     
+                        new_word5 = ''.join([l,i,j])
+                        new_word6 = ''.join([l,j,i])
+                        ans.add(new_word1)
+                        ans.add(new_word2)
+                        ans.add(new_word3)
+                        ans.add(new_word4)
+                        ans.add(new_word5)
+                        ans.add(new_word6)
+
 print(len(ans))
