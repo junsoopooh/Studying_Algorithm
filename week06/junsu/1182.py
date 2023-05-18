62156e50ef4e6e4579ecd3b9ea1e821cb95d8ecb
junsoopooh <junsoopooh@Naver.com>
2023-05-18 14:44:51 +0900

    aftermeeting

--- week06/junsu/1182.py
+++ week06/junsu/1182.py
@@ -2,42 +2,44 @@ import sys
 
 n, s = map(int, sys.stdin.readline().split())
 nums = list(map(int, sys.stdin.readline().split()))
-visited = [False]*n
+ans = []
 cnt = 0
+visited = [False]*n
 
-def pick(x, tmp):
-    global cnt
-    if x == 0:
-        if tmp == s:
-            cnt += 1
-            return
-        else:
-            return
-    
-    for i in range(n):
-        if not visited[i]:
-            tmp += nums[i]
-            visited[i] = True
-            pick(x-1, tmp+nums[i])
-
-for i in range(n):
-    visited = [False]*n
-    pick(i, 0)
-print(cnt)
-
-# def dfs(tmp):
+# 정답 코드
+# def solve(start):
 #     global cnt
-#     if tmp == s:
+#     if sum(ans) == s and len(ans) > 0:
 #         cnt += 1
-#         return
+    
+#     for i in range(start,n):
+#         ans.append(nums[i])
+#         solve(i+1)
+#         ans.pop()
+        
+# solve(0)
+# print(cnt)
 
+# 배준수 오답 코드
+# def pick(x, tmp):
+#     global cnt
+#     if x == 0:
+#         if tmp == s:
+#             cnt += 1
+#             return
+#         else:
+#             return
+    
 #     for i in range(n):
 #         if not visited[i]:
-#             tmp += nums[i]
 #             visited[i] = True
-#             dfs(tmp)
-#             visited[i] = False
+#             tmp += nums[i]
+#             x -= 1
+#             pick(x,tmp)
+#             x += 1
 #             tmp -= nums[i]
 
-# dfs(0)
+# for i in range(1,n+1):
+#     visited = [False]*n
+#     pick(i, 0)
 # print(cnt)
