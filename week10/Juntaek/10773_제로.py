593d851621ff529c993170e41a655b855dff53c6
seojuntaek <jwutn908@gmail.com>
2023-06-12 10:36:58 +0900

    feat: 10773

--- week10/Juntaek/10773_제로.py
+++ week10/Juntaek/10773_제로.py
@@ -3,9 +3,9 @@
 n = int(input())
 num_list = []
 for _ in range(n):
-    num = input()
-    if num == '0':
+    num = int(input())
+    if num == 0:
         num_list.pop()
     else:
         num_list.append(num)
-print(len(num_list))
+print(sum(num_list))
