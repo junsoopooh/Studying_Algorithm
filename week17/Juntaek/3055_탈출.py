77f2e1712a42f183b134ed85ce4a50ef1902d8f1
seojuntaek <jwutn908@gmail.com>
2023-08-30 13:15:45 +0900

    fixed: 3055, 2294

--- week17/Juntaek/3055_탈출.py
+++ week17/Juntaek/3055_탈출.py
@@ -41,44 +41,46 @@
 #       queue.append((i, j))
 
 # 풀이코드
-from collections import deque
+import collections
 import sys
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
 
-graph = [list(input().rstrip()) for _ in range(n)]
-distance = [[0] * m for _ in range(n)]
-dx, dy = [-1, 1, 0, 0], [0, 0, 1, -1]
-queue = deque()
+graph = [list(input().strip()) for _ in range(n)]
+distance = [[0] *m for _ in range(n)]
+dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
+queue = collections.deque()
 
-def bfs(Dx, Dy):
-  while queue:
-    x, y = queue.popleft()
-    if graph[Dx][Dy] == 'S':
-      return distance[Dx][Dy]
-    for i in range(4):
-      nx = x + dx[i]
-      ny = y + dy[i]
-      if 0 <= nx < n and 0 <= ny < m:
-        if (graph[nx][ny] == '.' or graph[nx][ny] == 'D') and graph[x][y] == 'S':
-          distance[nx][ny] = distance[x][y] + 1
-          queue.append((nx, ny))
-        elif (graph[nx][ny] == '.' or graph[nx][ny] == 'S') and graph[x][y] == '*':
-          graph[nx][ny] = '*'
-          queue.append((nx, ny))
-  return "KAKTUS"
+def bfs(Dx,Dy):
+    while queue:
+        x,y = queue.popleft()
+        if graph[Dx][Dy] == 'S':
+            return distance[Dx][Dy]
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < n and 0 <= ny < m:
+                if (graph[nx][ny] == '.' or graph[nx][ny] == 'D') and graph[x][y] == 'S':
+                    graph[nx][ny] = 'S'
+                    distance[nx][ny] = distance[x][y] + 1
+                    queue.append((nx,ny))
+                elif (graph[nx][ny] =='.' or graph[nx][ny] =='S') and graph[x][y] == '*':
+                    graph[nx][ny] = '*'
+                    queue.append((nx,ny))
+    return "KAKTUS"
 
-for x in range(n):
-  for y in range(m):
-    if graph[x][y] == 'S':
-      queue.append((x, y))
-    elif graph[x][y] == 'D':
-      Dx, Dy = x, y
 
 for x in range(n):
-  for y in range(m):
-    if graph[x][y] == '*':
-      queue.append((x, y))
+    for y in range(m):
+        if graph[x][y] == 'S':
+            queue.append((x,y))
+        elif graph[x][y] == 'D':
+            Dx,Dy = x,y
 
-print(bfs(Dx, Dy))
+for x in range(n):
+    for y in range(m):
+        if graph[x][y] == '*':
+            queue.append((x,y))
+
+print(bfs(Dx,Dy))
