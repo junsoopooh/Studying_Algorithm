dad9e72699c609e0c85baef2fcb0bee000f609eb
junsoopooh <junsoopooh@Naver.com>
2023-09-27 16:04:10 +0900

    update

--- week20/junsu/1946_2.py
+++ week20/junsu/1946_2.py
@@ -4,11 +4,9 @@ t = int(sys.stdin.readline())
 for _ in range(t):
     n = int(sys.stdin.readline())
     arr = [0 for _ in range(n+1)]
-    board = []
     for _ in range(n):
         a, b = map(int, sys.stdin.readline().split())
         arr[a] = b
-        board.append([a, b])
     cnt = 0
     for i in range(1, n+1):
         if i == 1:
