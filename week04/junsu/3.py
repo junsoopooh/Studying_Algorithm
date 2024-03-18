527e32c6a8dd6b46f81ea8b5f87b508adf8caa96
junsu <importunate.dev@gmail.com>
2024-03-18 15:50:50 +0900

    0318 3번

---
+++ week04/junsu/3.py
@@ -0,0 +1,53 @@
+#
+# 1차 80/100
+def solution(cacheSize, cities):
+    answer = 0
+    if not cacheSize:
+        answer = 5 * len(cities)
+        return answer
+    arr = []
+    for i in cities:
+        city = i.lower()
+        if len(arr) < cacheSize:
+            answer += 5
+            arr.append(city)
+            continue
+
+        if city in arr:
+            answer += 1
+            arr.remove(city)
+            arr.append(city)
+            continue
+        else:
+            answer += 5
+            arr.pop(0)
+            arr.append(city)
+    return answer
+
+
+# 2차 100/100
+
+
+def solution(cacheSize, cities):
+    answer = 0
+    if not cacheSize:
+        answer = 5 * len(cities)
+        return answer
+    arr = []
+    for i in cities:
+        city = i.lower()
+        if len(arr) < cacheSize and city not in arr:
+            answer += 5
+            arr.append(city)
+            continue
+
+        if city in arr:
+            answer += 1
+            arr.remove(city)
+            arr.append(city)
+            continue
+        else:
+            answer += 5
+            arr.pop(0)
+            arr.append(city)
+    return answer
